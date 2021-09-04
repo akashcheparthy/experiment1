53609ec0c2505fdc557cc0a9eb68be97b52a1313
akashcheparthy <85629176+akashcheparthy@users.noreply.github.com>
2021-09-04 08:04:57 -0700

    Add files via upload

---
+++ experiment1.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("My mane is Cheparthy Akash")
+
+
+# In[2]:
+
+
+print("My Roll No:A20405219055")
+
+
+# In[3]:
+
+
+20+14
+
+
+# In[4]:
+
+
+print("Hello")
+
+
+# In[5]:
+
+
+45-20
+
+
+# In[6]:
+
+
+8*3
+
+
+# In[7]:
+
+
+8**3
+
+
+# In[8]:
+
+
+9/2
+
+
+# In[9]:
+
+
+9//2
+
+
+# In[10]:
+
+
+print("Rohan's age is",20)
+
+
+# In[11]:
+
+
+print("I","am","B.tech Student","year",3)
+
+
+# In[12]:
+
+
+a=20
+
+
+# In[13]:
+
+
+print("value of a is",a)
+
+
+# In[14]:
+
+
+b=20
+c=30
+print(a,b,c)
+
+
+# In[15]:
+
+
+a=2000
+print(type(a))
+
+
+# In[16]:
+
+
+d="m"
+print(type(d))
+
+
+# In[17]:
+
+
+d='m'
+print(type(d))
+
+
+# In[18]:
+
+
+A=2.5 
+
+print(A) 
+
+print(type(A))
+
+
+# In[19]:
+
+
+Status=True 
+
+print(Status) 
+
+print(type(Status))
+
+
+# In[20]:
+
+
+A=50 
+
+
+# In[21]:
+
+
+B=50
+
+
+# In[22]:
+
+
+print(A)
+
+
+# In[23]:
+
+
+print(B)
+
+
+# In[25]:
+
+
+print(id(A)) 
+
+
+# In[26]:
+
+
+print(id(B)) 
+
+
+# In[27]:
+
+
+print(A,B) 
+
+
+# In[28]:
+
+
+print(id(A),id(B)) 
+
+
+# In[29]:
+
+
+print(int(45.67)) 
+
+
+# In[30]:
+
+
+print(float(45)) 
+
+
+# In[32]:
+
+
+print(int('123'))
+
+
+# In[33]:
+
+
+print(str("123"))
+
+
+# In[34]:
+
+
+#1.write a python program to print 'Welcome'
+print("welcome")
+
+
+# In[36]:
+
+
+#2.Write a Python Program to initialize all Numeric Data type variables and display.
+a=1
+b=1.1
+c=2+3j
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+
+
+# In[39]:
+
+
+#3.Write python program to use all arithmetic operators. 
+x=10
+y=20
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+
+# In[41]:
+
+
+#4.Write python program to use all relational operators.
+a=19
+b=20
+print('a is',a)
+print('b is',b)
+print("a>b is",a>b)
+print("a<b is ",a<b)
+
+
+# In[50]:
+
+
+#5.Write python program to use all logical operators. 
+a=True
+b=False
+print("and operator",a and b)
+print("or operator",a or b)
+print("nand operator",not(a and b))
+print("not operator a",not a)
+print("not operator b",not b)
+
+
+# In[ ]:
+
+
+
+
